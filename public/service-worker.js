4d8c04183816a2df51b964410e05ed72cecdf48f
AlexHirshFish <dimasikkotovstr@gmail.com>
2026-07-25 11:41:16 +0300

    Dim unavailable cards behind stop-list stamp

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'brooklyn-kiosk-v40';
+const CACHE = 'brooklyn-kiosk-v41';
 const MENU_IMAGES = Array.from({ length: 114 }, (_, index) => `./images/menu/${index}.webp`);
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', ...MENU_IMAGES];
 
